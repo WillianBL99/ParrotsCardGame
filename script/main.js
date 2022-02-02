e27dff5eda00a3a6d3e3c8a52f6e10689e0f42b9
Paulo Uilian <uilianlbarros@gmail.com>
2022-02-02 14:34:19 -0300

    Aperfeisoará o método para entrada da quantidade de cartas
    Cria uma function exclusiva para verificar se o número é par e está entre 3 e 15 e outra para repetir enquanto o da primeira for falso

--- script/main.js
+++ script/main.js
@@ -1,21 +1,28 @@
 window.onload = function(){
+    // monta as cartas com a quantidade passada pelo usuário em 'checkNumber()'
     buildGame(checkNumber());
 }
 
-
+// Pega o conteiner onde ficarão as cartas
 const appElement = document.querySelector("main");
+// cria o vertor com os nomes das imagens disponíveis
 const images = ["bobross","explody","fiesta","metal","revertit","triplets","unicorn"];
 
+// monta as cartas na tela
 function buildGame(num){
+    // embaralha as imagens no vetor
     images.sort(comparador);
     auxImages = [];
 
+    // insere os pares de imagens no array
     for(i = 0; i < num/2; i++){
         auxImages.push(images[i]);
         auxImages.push(images[i]);
     }
 
+    // embaralha as imagens novamente
     auxImages.sort(comparador).sort(comparador);
+    // para cada imagens do vertor é criado e inserido um cartão na tela
     auxImages.forEach( img => appElement.appendChild(addCard(img)));
 }
 
@@ -25,29 +32,31 @@ function buildGame(num){
 
 /* Cria uma carta com o gif do fundo passado pelo parametro 'gif' */
 function addCard(gif){
-    // -- criando elementos --
+    // cria a div principal da carta
     const divCard = document.createElement("div");
     divCard.setAttribute('class', 'card');
-
+    // cria a parte frontal da carta
     const front = document.createElement("div");
     front.setAttribute('class', 'face front');
-
+    // cria a parte de trás da carta
     const back = document.createElement("div");
     back.setAttribute('class', 'face back');
 
     const imgParrot = document.createElement("img");
     const gifParrot = document.createElement("img")
 
-    // -- interligando elementos
+    // Insere as imagens no front e no back da carta
     imgParrot.setAttribute('src', './img/front.png');
     gifParrot.setAttribute('src', `./img/${gif}parrot.gif`);
 
+    // finaliza a montagem da carta
     front.appendChild(imgParrot);
     back.appendChild(gifParrot);
 
     divCard.appendChild(front);
     divCard.appendChild(back);
 
+    // retorna a carta completa
     return divCard;
 }
 
@@ -55,13 +64,22 @@ function addCard(gif){
 
 // Repete até o usuário inserir um número par entre 4 e 14
 function checkNumber(){
-    let pairNumber;
+    let PairNumber;
 
-    do {
-        pairNumber = parseInt(prompt('Com quantas cartas você quer jogar?'))
-        if(pairNumber < 4 || pairNumber > 14 || pairNumber%2 !== 0) alert('Insira um número par entre 4 e 14, inclusive.');
-    } while(pairNumber < 4 || pairNumber > 14 || pairNumber%2 !== 0);
+    while(true){
+        pairNumber = parseInt(prompt('Com quantas cartas você quer jogar?'));
+        if(isPair(pairNumber)) break;
+    }
+
     return pairNumber;
+}
+
+function isPair(pairNumber){
+    if(pairNumber < 4 || pairNumber > 14 || pairNumber%2 !== 0){        
+        alert('Insira um número par entre 4 e 14, inclusive.');
+        return false;
+    }
+    return true;
 }
 
 // Embaralhar array a partir do sort()
